60cd11679676b1df1f45c8a0539d2c1fe2ec0b8b
ZJISUOO <moonjs96@naver.com>
2020-03-22 05:44:10 +0900

    IoT 설계

---
+++ IoT design/17.py
@@ -0,0 +1,8 @@
+c = 3.14 + 2.73j
+c.real # real part
+c.img # imaginary part
+c.conjugate() # conjugate of A + Bj is A - Bj
+c * 2 # multiplication is allowed
+c ** 2 # power operation as well
+d = 1 + ij # addtion and subtraction as well
+c - d
