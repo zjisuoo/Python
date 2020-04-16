dd69ee20bd6fded89a1b923ac68009349821392f
ZJISUOO <moonjs96@naver.com>
2020-04-16 20:54:35 +0900

    IoT-design-RaspberryPi 실습 내용 추가

--- DataScience/Statistics/correlation.py
+++ DataScience/Statistics/correlation.py
@@ -1,12 +1,12 @@
 from collections import Counter
 # from scratch.linear_algebra import sum_of_squares, dot
 from typing import List
+import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-num_friends = [100, 90, 49, 41, 45, 39, 19, 17, 15, 13, 11, 33, 39, 51, 71, 91, 30, 7, 16, 81]
-daily_minutes = [10, 20, 30, 40, 50, 60, 70, 15, 25, 45, 35, 24, 23, 13, 14, 15, 11, 9, 10, 11]
-daily_hours = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 6, 7, 8]
+num_friends = [100, 90, 50, 35, 25]
+daily_hours = [1, 2, 3, 4, 5]
 
 num_points = len(num_friends)
 largest_value = max(num_friends)
@@ -24,7 +24,7 @@ print('중심 경향성:', mean(num_friends))
 
 # 분산 = 산포도를 측정
 def de_mean(xs : List[float]) -> List[float] :
-    x_bar = mean(xs) # x 의 모든 데이터 포인트에서 평균을 뺸다.
+    x_bar = np.mean(xs) # x 의 모든 데이터 포인트에서 평균을 뺸다.
     return [ x - x_bar for x in xs]
 
 def variance(xs : List[float]) -> float :
@@ -45,15 +45,43 @@ def interquartile_range(xs : List[float]) -> float :
 def covariance(xs : List[float], ys : List[float]) -> float :
     assert len(xs) == len(ys), "xs and ys must have same number of elements"
 
-    return dot(de_mean(xs), de_mean(ys))/(len(xs) - 1)
+    return np.dot(de_mean(xs), de_mean(ys))/(len(xs) - 1)
 
 # 상관관계
 def correlation(xs : List[float], ys : List[float]) -> float :
-    stdev_x = standard_deviation(xs)
-    stdev_y = standard_deviation(ys)
+    stdev_x = np.std(xs)
+    stdev_y = np.std(ys)
     if stdev_x > 0 and stdev_y > 0 :
         return covariance(xs, ys) / stdev_x / stdev_y
     else :
         return 0
-    
-    print(correlation(num_friends, daily_minutes))
+
+# 상관계수 1.2499999999999998
+daily_minutes = [200, 180, 100, 70, 50]
+plt.plot(num_friends, daily_minutes, 'bo')
+plt.xlabel('num_friends')
+plt.ylabel('daily_minutes')
+plt.xticks([0, 50, 100, 150, 200])
+plt.yticks([0, 50, 100, 150, 200])
+plt.show()
+print('상관계수:', correlation(num_friends, daily_minutes))
+
+# 상관계수 0.5725733554122433
+daily_minutes = [30, 40, 50, 20, 15]
+plt.plot(num_friends, daily_minutes, 'bo')
+plt.xlabel('num_friends')
+plt.ylabel('daily_minutes')
+plt.xticks([0, 25, 50, 75, 100])
+plt.yticks([0, 25, 50, 75, 100])
+plt.show()
+print('상관계수:', correlation(num_friends, daily_minutes))
+
+# 상관계수 -1.1797752808988764
+daily_minutes = [50, 70, 100, 180, 200]
+plt.plot(num_friends, daily_minutes, 'bo')
+plt.xlabel('num_friends')
+plt.ylabel('daily_minutes')
+plt.xticks([0, 25, 50, 75, 100])
+plt.yticks([0, 50, 100, 150, 200])
+plt.show()
+print('상관계수:', correlation(num_friends, daily_minutes))
