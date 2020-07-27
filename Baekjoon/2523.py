a16148bbc9cea31d97420f202f4efef45c1460fc
ZJISUOO <moonjs96@naver.com>
2020-07-28 03:13:21 +0900

    백준 13까지

--- Baekjoon/2523.py
+++ Baekjoon/2523.py
@@ -1,9 +1,9 @@
 star = int(input())
 
-for i in range(1, star + 1, 1) :
+for i in range(1, star, 1) :
     print('*' * i)
 
-for i in range(star + 1, 0 , -1) :
+for i in range(star, 0 , -1) :
     print('*' * i)
 
 try :
